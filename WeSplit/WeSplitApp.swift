5ccd8aba734bb136ec3c73ff7c6fa782f1c6c13b
Takasur Azeem <takasurazeem@gmail.com>
2022-02-17 23:16:57 +0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Takasur Azeem on 17/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
