e5d1baa057654c6e0d7709f118609474abfe67e8
Takasur Azeem <takasurazeem@gmail.com>
2022-02-18 15:47:29 +0500

    Calculates total per person
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -10,10 +10,21 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var checkAmount = 0.0
-    @State private var nunmberOfPeople = 0
+    @State private var numberOfPeople = 0
     @State private var tipPercentage = 20
     
     let tipPercengates = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView {
@@ -22,7 +33,7 @@ struct ContentView: View {
                     TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                         .keyboardType(.decimalPad)
                     
-                    Picker("Number of people", selection: $nunmberOfPeople) {
+                    Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
                         }
@@ -38,6 +49,9 @@ struct ContentView: View {
                     .pickerStyle(.segmented)
                 } header: {
                     Text("How much tip do you want to leave?")
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
                 }
             }
             .navigationTitle("WeSplit")
