345e9c07a825efb333d11a1823e74bcb370cd06d
Takasur Azeem <takasurazeem@gmail.com>
2022-02-18 15:13:04 +0500

    Adds currency textfiled for amount
    Signed-off-by: Takasur Azeem <takasurazeem@gmail.com>

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.0
+    @State private var nunmberOfPeople = 0
+    @State private var tipPercentage = 20
+    
+    let tipPercengates = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                        .keyboardType(.decimalPad)
+                    
+                }
+            }
     }
 }
 
